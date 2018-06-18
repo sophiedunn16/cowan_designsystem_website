512aa36657ee0430e517c8b1c39bfb86a618144d
Sophie <sophie.dunn16@gmail.com>
2018-06-18 16:17:21 +0800

    Added Fade In Scroll JS

---
+++ _site/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  // Attach Scroll to Window //
+  window.sr = ScrollReveal();
+
+  //Attach ScrollReveal to the elements //
+
+  sr.reveal ('.show-me', { duration: 1500 });
+
+});
