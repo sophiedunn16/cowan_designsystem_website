005c50d1671b259add3e87de2700ff665568800a
Sophie <sophie.dunn16@gmail.com>
2018-06-19 09:32:20 +0800

    Commit pre-pres

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+  /*
+  ** Toggle's body class
+  ** change the CSS accordingly: body.menu-visible for a visible menu - body.menu-hidden for a hidden menu
+  */
+  function toggleMenu(event) {
+
+    // Prevent following the a link
+    event.preventDefault();
+
+    // Change the body class
+    $('body').toggleClass('menu-visible menu-hidden');
+
+  }
+
+  // Bind click event to the hamburger button
+  $('body').on('click', '.toggle-menu', toggleMenu);
+
+
   // Attach Scroll to Window //
   window.sr = ScrollReveal();
 
